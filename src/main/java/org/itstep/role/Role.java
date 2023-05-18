c09a6927be7aaaed01d55f10efa71cb4b95e09c1
gk <officialkepik@gmail.com>
2023-05-18 21:13:26 +0300

    j15.05

---
+++ src/main/java/org/itstep/role/Role.java
@@ -0,0 +1,16 @@
+package org.itstep.role;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/org/itstep/user/UserRepository.java
@@ -0,0 +1,13 @@
+package org.itstep.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //JPQL
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    public User getUserByUsername(@Param("username") String username);
+}
